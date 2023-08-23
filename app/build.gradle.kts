5b0faf7cc63dcce28be8efa49f5f07717ca1c217
kemal.yuki <kemalgeylaniyuki@gmail.com>
2023-08-23 09:03:55 +0300

    Project Created

---
+++ app/build.gradle.kts
@@ -0,0 +1,116 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id("androidx.navigation.safeargs")
+
+}
+
+android {
+    namespace = "com.example.mvvmcountries"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.mvvmcountries"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+
+}
+
+val lifecycle_version = "2.6.1"
+val room_version = "2.5.2"
+val nav_version = "2.7.0"
+val retrofitVersion = "2.9.0"
+val rxJavaVersion = "2.2.9"
+val rxAndroidVersion = "2.1.1"
+val glideVersion = "4.11.0"
+val supportVersion = "28.0.0"
+val preferencesVersion = "1.2.1"
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.10.1.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+    // Annotation processor
+    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycle_version")
+    // optional - helpers for implementing LifecycleOwner in a Service
+    implementation("androidx.lifecycle:lifecycle-service:$lifecycle_version")
+    // optional - ProcessLifecycleOwner provides a lifecycle for the whole application process
+    implementation("androidx.lifecycle:lifecycle-process:$lifecycle_version")
+    // optional - ReactiveStreams support for LiveData
+    implementation("androidx.lifecycle:lifecycle-reactivestreams-ktx:$lifecycle_version")
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    //coroutines for threads
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+
+    implementation ("io.reactivex.rxjava2:rxjava:$rxJavaVersion")
+    implementation ("io.reactivex.rxjava2:rxandroid:$rxAndroidVersion")
+
+    implementation ("com.github.bumptech.glide:glide:$glideVersion")
+
+    //noinspection GradleCompatible
+    implementation ("com.android.support:palette-v7:$supportVersion")
+    //noinspection GradleCompatible
+    implementation ("com.android.support:design:$supportVersion")
+
+    //preference
+    implementation ("androidx.preference:preference:$preferencesVersion")
+
+    implementation ("com.google.android.material:material:1.9.0")
+
+}
